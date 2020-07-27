53b1dbeaad437098cca006e3c81be4bca256322e
Michael JunDundas <michael.jundundas@Michael-Jun-Dundas-MacBook.local>
2020-07-27 08:27:50 -0500

    first steps for stripe integration

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,12 @@ has_person_name
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+
+ devise :database_authenticatable, :registerable,
+      :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:stripe_connect]
   has_many :projects, dependent: :destroy
+
+  def can_receive_payments?
+    uid? && provider? && access_code? && publishable_key?
+  end
 end

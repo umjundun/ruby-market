b5e0cb1963cb4f08848eb26456b7673744096d1a
Michael JunDundas <michael.jundundas@Michael-Jun-Dundas-MacBook.local>
2020-07-27 11:42:21 -0500

    Stripe integration looks to be working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,13 @@ module ApplicationHelper
         user_signed_in? && current_user.admin?
     end
 
-  def stripe_url
-    "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#
-    {Rails.application.credentials.dig(:stripe, :connect_client_id)}&scope=read_write"
-  end
+    def stripe_url
+    "https://connect.stripe.com/oauth/authorize?response_type=code&client_id=#{Rails.application.credentials.dig(:stripe, :connect_client_id)}&scope=read_write"
+    end
+
+    def stripe_connect_button # add this method
+      link_to stripe_url, class: "btn-stripe-connect" do
+        content_tag :span, "Connect Stripe"
+      end
+    end
 end
